a1bb08e2b157aecaac8c882f6f664c24650170ec
JeffAllison <itsjallison@gmail.com>
2022-05-12 17:25:02 -0700

    imported link form RRD into album.js, passed destructed props into component Album, rendered album by ID with ptags for each SBvalue & prop

--- src/Album.js
+++ src/Album.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Album() {
-  return <div>Album</div>;
+export default function Album({
+  album: { id, title, artist, released, minutes, tracks, producer, genres },
+}) {
+  return (
+    <Link to={`/albums/${id}`}>
+      <div className="album">
+        <p>{title}</p>
+        <p>{artist}</p>
+        <p>{released}</p>
+        <p>{minutes}</p>
+        <p>{tracks}</p>
+        <p>{producer}</p>
+        <p>{genres}</p>
+      </div>
+    </Link>
+  );
 }
