3868a6a93da5ae4086dd0e9f25d8e15280830abc
JeffAllison <itsjallison@gmail.com>
2022-05-12 18:12:09 -0700

    imported state, createalbum, and usehistory into createpage.js.  set history and the formAlbum with values from SB tabel, created handlesubmitAlbum component and called history in the function to push submitted album to album list

--- src/CreatePage.js
+++ src/CreatePage.js
@@ -1,5 +1,25 @@
-import React from 'react';
+import { useState } from 'react';
+import { createAlbum } from './services/fetch-utils';
+import { useHistory } from 'react-router-dom';
 
 export default function CreatePage() {
+  const history = useHistory();
+  const [formAlbum, setFormAlbum] = useState({
+    title: '',
+    artist: '',
+    released: '',
+    minutes: '',
+    tracks: '',
+    producer: '',
+    genres: '',
+  });
+
+  async function handleSubmitAlbum(e) {
+    e.preventDefault();
+
+    await createAlbum(formAlbum);
+
+    history.push('/albums');
+  }
   return <div>CreatePage</div>;
 }
