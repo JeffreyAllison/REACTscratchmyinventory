4856a8fa89f13044c880745d17bc1e4455867c44
JeffAllison <itsjallison@gmail.com>
2022-05-12 17:38:12 -0700

    imported useState and Effect from react, getAlbums from fetchutils, and Album from Album.js to listpage.js.  created listpage component and set state for albums nad set albums, added use effect to fetch the albums list

--- src/ListPage.js
+++ src/ListPage.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { getAlbums } from './services/fetch-utils';
+import Album from './Album';
 
 export default function ListPage() {
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    async function fetch() {
+      const returnAlbums = await getAlbums();
+
+      setAlbums(returnAlbums);
+    }
+    fetch();
+  }, []);
+
   return <div>ListPage</div>;
 }
