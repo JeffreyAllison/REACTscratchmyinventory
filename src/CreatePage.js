d80164ec9983d08ac208b75c903569fb529ce3c3
JeffAllison <itsjallison@gmail.com>
2022-05-13 09:37:15 -0700

    added create album form to render created album, added inputs for each SB value and an onchange function to update DOM when state is called

--- src/CreatePage.js
+++ src/CreatePage.js
@@ -21,5 +21,68 @@ export default function CreatePage() {
 
     history.push('/albums');
   }
-  return <div>CreatePage</div>;
+  return (
+    <div className="create-album-form">
+      <form onSubmit={handleSubmitAlbum}>
+        <h2>Create an Album for the Inventory</h2>
+        <label>
+          <input
+            value={formAlbum.title}
+            type="text"
+            onChange={(e) => setFormAlbum({ ...formAlbum, title: e.target.value })}
+            name="title"
+          />
+        </label>
+        <label>
+          <input
+            value={formAlbum.artist}
+            type="text"
+            onChange={(e) => setFormAlbum({ ...formAlbum, artist: e.target.value })}
+            name="artist"
+          />
+        </label>
+        <label>
+          <input
+            value={formAlbum.released}
+            type="text"
+            onChange={(e) => setFormAlbum({ ...formAlbum, released: e.target.value })}
+            name="released"
+          />
+        </label>
+        <label>
+          <input
+            value={formAlbum.minutes}
+            type="text"
+            onChange={(e) => setFormAlbum({ ...formAlbum, minutes: e.target.value })}
+            name="minutes"
+          />
+        </label>
+        <label>
+          <input
+            value={formAlbum.tracks}
+            type="text"
+            onChange={(e) => setFormAlbum({ ...formAlbum, tracks: e.target.value })}
+            name="tracks"
+          />
+        </label>
+        <label>
+          <input
+            value={formAlbum.producer}
+            type="text"
+            onChange={(e) => setFormAlbum({ ...formAlbum, producer: e.target.value })}
+            name="producer"
+          />
+        </label>
+        <label>
+          <input
+            value={formAlbum.genres}
+            type="text"
+            onChange={(e) => setFormAlbum({ ...formAlbum, genres: e.target.value })}
+            name="genres"
+          />
+        </label>
+        <button>Add Album to Inventory</button>
+      </form>
+    </div>
+  );
 }
